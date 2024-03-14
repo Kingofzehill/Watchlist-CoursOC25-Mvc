a66574fe43227a60b2e4be4a3bbeec9c83626788
KingOfZehill <kingofzehill@gmail.com>
2024-03-14 15:02:30 +0100

    Retry add-magiration and update-database for testing if it resolves the bug004 issue by regenerating applicationDbContext Then solution rebuild and new debugage test (running app).

---
+++ Watchlist/Data/Migrations/20240314135932_AfterUtilisateurClassUPDWithKey.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Watchlist.Data.Migrations
+{
+    public partial class AfterUtilisateurClassUPDWithKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ModeleVueFilm",
+                columns: table => new
+                {
+                    IdFilm = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Annee = table.Column<int>(type: "int", nullable: false),
+                    PresentDansListe = table.Column<bool>(type: "bit", nullable: false),
+                    Vu = table.Column<bool>(type: "bit", nullable: false),
+                    Note = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ModeleVueFilm", x => x.IdFilm);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ModeleVueFilm");
+        }
+    }
+}
