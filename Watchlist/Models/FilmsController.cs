072dce5b7d9c7f7ec86013fa49a0ce0cf7a119da
KingOfZehill <kingofzehill@gmail.com>
2024-03-18 19:06:59 +0100

    UPD010: Permet l'accès au UserManager pour accéder à l'utilsateur connecté. UPD011: affiche la liste des films de l'utilisateur connecté en utilisant le modèle : ModeleVueFilm

--- Watchlist/Models/FilmsController.cs
+++ Watchlist/Models/FilmsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -12,18 +13,65 @@ namespace Watchlist.Models
     public class FilmsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        //UPD010: Permet l'accès au UserManager pour accéder à l'utilsateur connecté.
+        private readonly UserManager<Utilisateur> _gestionnaire;
 
-        public FilmsController(ApplicationDbContext context)
+        public FilmsController(ApplicationDbContext context, UserManager<Utilisateur> gestionnaire)
         {
             _context = context;
+            //UPD010:  variable permettant d'accéder au UserManager.
+            _gestionnaire = gestionnaire; 
         }
 
+        //UPD010: récupère l'id de l'utilisateur connecté
+        [HttpGet]
+        public async Task<string> RecupererIdUtilisateurCourant()
+        {
+            Utilisateur utilisateur = await GetCurrentUserAsync();
+            return utilisateur?.Id;
+        }
+
+        //UPD010: GetCurrentUserAsync appelle la méthode GetUserAsync du userManager contextuel de l'app
+        private Task<Utilisateur> GetCurrentUserAsync() =>
+        _gestionnaire.GetUserAsync(HttpContext.User);
+
+        //UPD011: affiche la liste des films de l'utilisateur connecté en utilisant le modèle : ModeleVueFilm
         // GET: Films
         public async Task<IActionResult> Index()
         {
+            //Old code before UPD011
+            /*
               return _context.Films != null ? 
                           View(await _context.Films.ToListAsync()) :
                           Problem("Entity set 'ApplicationDbContext.Films'  is null.");
+            */
+            
+            //UPD011: récupère l'id utilisateur connecté.
+            var idUtilisateur = await RecupererIdUtilisateurCourant();
+            //LinQ : select des films. 
+            var modele = await _context.Films.Select(x =>
+                    new ModeleVueFilm
+                    {
+                        IdFilm = x.Id,
+                        Titre = x.Titre,
+                        Annee = x.Annee
+                    }).ToListAsync();
+
+            //Parcours les films.
+            foreach (var item in modele)
+            {
+                //Parcours le DbSet (jeu d'enregistrement) des films (FilmsUtlisateur, voir fichier dans le dossier Data de l'app dans Visual Studio)
+                //pour l'utilisateur connecté et le film sélectionné
+                var m = await _context.FilmsUtilisateur.FirstOrDefaultAsync(x =>
+                           x.IdUtilisateur == idUtilisateur && x.IdFilm == item.IdFilm);
+                if (m != null)
+                {
+                    item.PresentDansListe = true;
+                    item.Note = m.Note;
+                    item.Vu = m.Vu;
+                }
+            }
+            return View(modele);
         }
 
         // GET: Films/Details/5
@@ -55,6 +103,7 @@ namespace Watchlist.Models
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        // NOTE001 : La liste Bind définit les champs contrôlés 
         public async Task<IActionResult> Create([Bind("Id,Titre,Annee")] Film film)
         {
             //NOTE001 : vérification de la validité du formulaire avant création de l'enregristrement
